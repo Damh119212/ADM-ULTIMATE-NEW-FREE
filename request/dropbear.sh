7e9735937ca41d9fe900ce9440f5019504e79077
Damh119212 <36243830+Damh119212@users.noreply.github.com>
2018-11-04 08:33:24 -0500

    Update dropbear.sh

--- request/dropbear.sh
+++ request/dropbear.sh
@@ -143,7 +143,7 @@ EOF
 for dpts in $(echo $PORT); do
 sed -i "s/VAR/-p $dpts VAR/g" /etc/default/dropbear
 done
-sed -i "s/VAR/g" /etc/default/dropbear
+sed -i "s/VAR//g" /etc/default/dropbear
 } || {
 echo -e "Port 22
 Protocol 2
@@ -187,7 +187,7 @@ EOF
 for dpts in $(echo $PORT); do
 sed -i "s/VAR/-p $dpts VAR/g" /etc/default/dropbear
 done
-sed -i "s/VAR/g" /etc/default/dropbear
+sed -i "s/VAR//g" /etc/default/dropbear
 }
 fun_eth
 service ssh restart > /dev/null 2>&1
