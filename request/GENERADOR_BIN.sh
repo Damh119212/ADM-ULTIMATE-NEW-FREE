ddfde99deadae2b1444fe1fdd7c1946cc8fac23b
Damh119212 <36243830+Damh119212@users.noreply.github.com>
2018-11-03 20:10:01 -0500

    Update GENERADOR_BIN.sh

--- request/GENERADOR_BIN.sh
+++ request/GENERADOR_BIN.sh
@@ -27,7 +27,7 @@ else
 echo "${texto[$@]}"
 fi
 }
-link_bin="https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/ADM-ULTIMATE-NEW-FREE/master/Install/generadorcc.py"
+link_bin="https://raw.githubusercontent.com/Damh119212/ADM-ULTIMATE-NEW-FREE/master/Install/generadorcc.py"
 [[ ! -e /usr/bin/generadorcc.py ]] && wget -O /usr/bin/generadorcc.py ${link_bin} > /dev/null && chmod +x /usr/bin/generadorcc.py
 msg -ama "$(fun_trans "GERADOR DE BINS OFICIAL")"
 msg -bar
